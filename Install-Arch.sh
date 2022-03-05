4d0f6f5261eb7d66743a3a13897864515968394c
Mutahar <matttrentham207@gmail.com>
2022-03-05 01:27:33 -0600

    better

--- Install-Arch.sh
+++ Install-Arch.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-#  ____ _____
-# / ___| ____|
-#| |  _|  _|
-#| |_| | |___
-# \____|_____|
 
+# this is a script that installs QEMU/KVM and everything else you will Need for a VM on Arch or An Arch Based Distro
 
-## this is a script that installs QEMU/KVM and everything else you will Need for a VM on Arch or An Arch Based Distro
-
-## Installing everything needed
+# Installing everything needed
 
 sudo pacman -S libvirt libvirt-glib libvirt-python virt-install virt-manager qemu qemu-arch-extra ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat iptables swtpm
 
-## starting LibVirt
+# starting LibVirt
 
 sudo systemctl enable libvirtd.service
 
@@ -22,20 +16,25 @@ sudo enable virtlogd.socket
 
 sudo start virtlogd.socket
 
-## turning on the Network
+# turning on the Network
 
 sudo virsh net-start default
 
-sudo virsh net-autostart default %
+sudo virsh net-autostart default 
 
-## Fixing Virsh
+# Fixing Virsh
 
 sudo cp -r libvirtd.conf /etc/libvirt/libvirt.conf
 sudo cp -r libvirtd.conf ~/.config/libvirt/libvirt.conf
 
-## end MSG
+# end MSG
 
-echo -
-echo -
-echo -
-echo FINISHED Just Reboot And You Can Use KVM/QEMU
+echo "A reboot is needed,type 'y' to reboot now and 'n' to reboot later"
+read YN
+
+if [ $YN = 'y' ]
+  then
+    reboot
+  else
+  echo "done, just reboot and your good to go!"
+fi
